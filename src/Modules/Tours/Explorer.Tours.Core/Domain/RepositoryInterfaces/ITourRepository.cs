89706d4de47a53718884fec437e71e92b515a8d2
Tamas Papp <pap.ra4.2022@uns.ac.rs>
2025-11-17 23:54:25 +0100

    Created necessary tour files

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/RepositoryInterfaces/ITourRepository.cs
@@ -0,0 +1,18 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain.RepositoryInterfaces;
+
+public interface ITourRepository
+{
+    Tour Create(Tour tour);
+    Tour Update(Tour tour);
+    Tour Get(long id);
+    void Delete(long id);
+    PagedResult<Tour> GetPaged(int page, int pageSize);
+    PagedResult<Tour> GetByCreatorId(long creatorId, int page, int pageSize);
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Administration/TourService.cs
@@ -0,0 +1,12 @@
+﻿using Explorer.Tours.API.Public.Administration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.UseCases.Administration;
+
+public class TourService: ITourService
+{
+}
