61d8f0a14a3a167306bcdc0cbb572962292f3a60
Tamas Papp <pap.ra4.2022@uns.ac.rs>
2025-11-17 23:54:25 +0100

    Created necessary tour files

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Tour.cs
@@ -0,0 +1,39 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain;
+
+public enum TourStatus
+{
+    Draft,
+    Published,
+    Archived
+}
+public class Tour : Entity
+{
+    long CreatorId { get; init; }
+    string Title { get; init; }
+    string Description { get; init; }
+    int Difficulty { get; init; }
+    string[] Tags { get; init; }
+    TourStatus Status { get; init; }
+    double Price { get; init; }
+
+    public Tour(long creatorId,string title, string description, int difficulty, string[] tags, TourStatus status=TourStatus.Draft, double price=0)
+    {
+        if (difficulty < 1 || difficulty > 10) throw new ArgumentException("Difficulty must be between 1 and 10.");
+        if (price < 0) throw new ArgumentException("Price cannot be negative.");
+        Title = title;
+        Description = description;
+        Difficulty = difficulty;
+        Tags = tags;
+        Status = status;
+        Price = price;
+        CreatorId = creatorId;
+    }
+
+}
---
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/Database/Repositories/TourDbRepository.cs
@@ -0,0 +1,53 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.Core.Domain;
+using Explorer.Tours.Core.Domain.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Infrastructure.Database.Repositories;
+
+public class TourDbRepository : ITourRepository
+{
+    protected readonly ToursContext dbContext;
+    private readonly DbSet<Tour> _dbSet;
+
+    public TourDbRepository(ToursContext dbContext)
+    {
+        this.dbContext = dbContext;
+        _dbSet = dbContext.Set<Tour>();
+    }
+
+    public Tour Create(Tour tour)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Tour Get(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public PagedResult<Tour> GetByCreatorId(long creatorId, int page, int pageSize)
+    {
+        throw new NotImplementedException();
+    }
+
+    public PagedResult<Tour> GetPaged(int page, int pageSize)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Tour Update(Tour tour)
+    {
+        throw new NotImplementedException();
+    }
+}
