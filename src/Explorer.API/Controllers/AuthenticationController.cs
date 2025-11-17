fee8899501ad177bbf3667d47c4967360fbf8094
Emi7i <emilijaopsenica@gmail.com>
2025-11-17 23:36:18 +0100

    Add API calls and some functionality

--- src/Explorer.API/Controllers/AuthenticationController.cs
+++ src/Explorer.API/Controllers/AuthenticationController.cs
@@ -1,9 +1,11 @@
 ﻿using Explorer.Stakeholders.API.Dtos;
 using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Explorer.API.Controllers;
 
+[Authorize]
 [Route("api/users")]
 [ApiController]
 public class AuthenticationController : ControllerBase
@@ -16,12 +18,28 @@ public class AuthenticationController : ControllerBase
     }
 
     [HttpPost]
+    [AllowAnonymous]
     public ActionResult<AuthenticationTokensDto> RegisterTourist([FromBody] AccountRegistrationDto account)
     {
         return Ok(_authenticationService.RegisterTourist(account));
     }
 
+    [HttpPost("register-admin")]
+    [Authorize(Policy = "administratorPolicy")]
+    public ActionResult<AuthenticationTokensDto> RegisterAdmin([FromBody] AccountRegistrationDto account)
+    {
+        return Ok(_authenticationService.RegisterAdmin(account));
+    }
+
+    [HttpPost("register-author")]
+    [Authorize(Policy = "administratorPolicy")]
+    public ActionResult<AuthenticationTokensDto> RegisterAuthor([FromBody] AccountRegistrationDto account)
+    {
+        return Ok(_authenticationService.RegisterAuthor(account));
+    }
+
     [HttpPost("login")]
+    [AllowAnonymous]
     public ActionResult<AuthenticationTokensDto> Login([FromBody] CredentialsDto credentials)
     {
         return Ok(_authenticationService.Login(credentials));
