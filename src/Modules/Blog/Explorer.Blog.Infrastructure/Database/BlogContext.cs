cb2725dbc55bf3794f22a0945a9ed4cb46ad0c29
chemi0 <milanspasojevic232@gmail.com>
2025-11-18 00:04:01 +0100

    feat: Repo/DbContext/Dependancy Injection
    Repozitorijum i DbContext konfigurisani
    Dependency injection je sređen

--- src/Modules/Blog/Explorer.Blog.Infrastructure/Database/BlogContext.cs
+++ src/Modules/Blog/Explorer.Blog.Infrastructure/Database/BlogContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Explorer.Blog.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using BlogEntity = Explorer.Blog.Core.Domain.Blog;
 
 namespace Explorer.Blog.Infrastructure.Database;
 
 public class BlogContext : DbContext
 {
+    public DbSet<BlogEntity> Blogs {get; set;} // Blogs table in DB
     public BlogContext(DbContextOptions<BlogContext> options) : base(options) {}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
