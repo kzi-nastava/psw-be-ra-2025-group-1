55045158944dfdff076f010d2d97b35bc92d9bca
Luka Kojović <kojovic.ra107.2022@uns.ac.rs>
2025-11-18 00:15:53 +0100

    Feat: Added MeetUp class, its DTO, service and repository interfaces, and service implementation.

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/MeetUpDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Explorer.Tours.API.Dtos
+{
+    public class MeetUpDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int Longitude { get; set; }
+        public int Latitude { get; set; }
+        public long UserId { get; set; }
+    }
+}
