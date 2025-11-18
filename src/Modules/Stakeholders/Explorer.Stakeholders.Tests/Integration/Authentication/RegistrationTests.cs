80a4ac1f5234210e06f98ae9a16eb7e3e2e1bf38
Emi7i <emilijaopsenica@gmail.com>
2025-11-18 23:47:26 +0100

    Add Registration tests for Author and Admins

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/Authentication/RegistrationTests.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/Authentication/RegistrationTests.cs
@@ -1,12 +1,14 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using Explorer.API.Controllers;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Explorer.Stakeholders.Core.Domain;
+using Explorer.Stakeholders.Infrastructure.Database;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
-using Explorer.API.Controllers;
-using Explorer.Stakeholders.API.Dtos;
-using Explorer.Stakeholders.API.Public;
-using Explorer.Stakeholders.Infrastructure.Database;
-using Explorer.Stakeholders.Core.Domain;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Explorer.Stakeholders.Tests.Integration.Authentication;
 
@@ -51,6 +53,140 @@ public class RegistrationTests : BaseStakeholdersIntegrationTest
         storedPerson.ShouldNotBeNull();
         storedPerson.UserId.ShouldBe(storedAccount.Id);
         storedPerson.Name.ShouldBe(account.Name);
+    }
+
+    [Fact]
+    public void Successfully_registers_admins()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<StakeholdersContext>();
+        var controller = CreateController(scope);
+
+        var existingAdmin = new CredentialsDto
+        {
+            Username = "admin@gmail.com",
+            Password = "admin",
+        };
+        var loginResponse = ((ObjectResult)controller.Login(existingAdmin).Result).Value as AuthenticationTokensDto;
+
+        // Dekodiramo Token da dobijemo userId
+        var decodedToken = new JwtSecurityTokenHandler().ReadJwtToken(loginResponse.AccessToken);
+        var userId = long.Parse(decodedToken.Claims.First(c => c.Type == "id").Value);
+
+        controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext
+            {
+                User = CreateAdminClaimsPrincipal(userId, existingAdmin.Username)
+            }
+        };
+
+        var newAdmin = new AccountRegistrationDto
+        {
+            Username = "newAdmin@gmail.com",
+            Email = "newAdmin@gmail.com",
+            Password = "admin456",
+            Name = "New",
+            Surname = "Admin"
+        };
+
+        // Act
+        var result = ((ObjectResult)controller.RegisterAdmin(newAdmin).Result).Value as AuthenticationTokensDto;
+
+        // Assert - Response
+        result.ShouldNotBeNull();
+        result.Id.ShouldNotBe(0);
+
+        var decodedAccessToken = new JwtSecurityTokenHandler().ReadJwtToken(result.AccessToken);
+        var personId = decodedAccessToken.Claims.FirstOrDefault(c => c.Type == "personId");
+        personId.ShouldNotBeNull();
+        personId.Value.ShouldNotBe("0");
+
+        // Assert - Database
+        dbContext.ChangeTracker.Clear();
+        var storedAccount = dbContext.Users.FirstOrDefault(u => u.Username == newAdmin.Username);
+        storedAccount.ShouldNotBeNull();
+        storedAccount.Role.ShouldBe(UserRole.Administrator);
+
+        var storedPerson = dbContext.People.FirstOrDefault(p => p.Email == newAdmin.Email);
+        storedPerson.ShouldNotBeNull();
+        storedPerson.UserId.ShouldBe(storedAccount.Id);
+        storedPerson.Name.ShouldBe(newAdmin.Name);
+    }
+
+    [Fact]
+    public void Successfully_registers_authors()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<StakeholdersContext>();
+        var controller = CreateController(scope);
+
+        var existingAdmin = new CredentialsDto
+        {
+            Username = "admin@gmail.com",
+            Password = "admin",
+        };
+        var loginResponse = ((ObjectResult)controller.Login(existingAdmin).Result).Value as AuthenticationTokensDto;
+
+        // Dekodiramo Token da dobijemo userId
+        var decodedToken = new JwtSecurityTokenHandler().ReadJwtToken(loginResponse.AccessToken);
+        var userId = long.Parse(decodedToken.Claims.First(c => c.Type == "id").Value);
+
+        controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext
+            {
+                User = CreateAdminClaimsPrincipal(userId, existingAdmin.Username) 
+            }
+        };
+
+        var newAuthor = new AccountRegistrationDto
+        {
+            Username = "autorA@gmail.com",
+            Email = "autorA@gmail.com",
+            Password = "autor645",
+            Name = "Novi",
+            Surname = "Autor"
+        };
+
+        // Act
+        var result = ((ObjectResult)controller.RegisterAuthor(newAuthor).Result).Value as AuthenticationTokensDto;
+
+        // Assert - Response
+        result.ShouldNotBeNull();
+        result.Id.ShouldNotBe(0);
+
+        var decodedAccessToken = new JwtSecurityTokenHandler().ReadJwtToken(result.AccessToken);
+        var personId = decodedAccessToken.Claims.FirstOrDefault(c => c.Type == "personId");
+        personId.ShouldNotBeNull();
+        personId.Value.ShouldNotBe("0");
+
+        // Assert - Database
+        dbContext.ChangeTracker.Clear();
+        var storedAccount = dbContext.Users.FirstOrDefault(u => u.Username == newAuthor.Username);
+        storedAccount.ShouldNotBeNull();
+        storedAccount.Role.ShouldBe(UserRole.Author);
+
+        var storedPerson = dbContext.People.FirstOrDefault(p => p.Email == newAuthor.Email);
+        storedPerson.ShouldNotBeNull();
+        storedPerson.UserId.ShouldBe(storedAccount.Id);
+        storedPerson.Name.ShouldBe(newAuthor.Name);
+    }
+
+    // Helper metoda za ClaimsPrincipal
+    private ClaimsPrincipal CreateAdminClaimsPrincipal(long userId, string username)
+    {
+        var claims = new List<Claim>
+    {
+        new Claim("id", userId.ToString()),
+        new Claim("username", username),
+        new Claim(ClaimTypes.Role, "Author")
+    };
+
+        var identity = new ClaimsIdentity(claims, "Test");
+        return new ClaimsPrincipal(identity);
     }
 
     private static AuthenticationController CreateController(IServiceScope scope)
