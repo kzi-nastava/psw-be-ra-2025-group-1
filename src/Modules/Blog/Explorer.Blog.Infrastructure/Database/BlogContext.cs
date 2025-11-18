e5671d3908ecc07b1078eb655f559f750073445e
chemi0 <milanspasojevic232@gmail.com>
2025-11-18 12:30:42 +0100

    test: Testovi za nove funckionalnosti definisani

--- src/Modules/Blog/Explorer.Blog.Infrastructure/Database/BlogContext.cs
+++ src/Modules/Blog/Explorer.Blog.Infrastructure/Database/BlogContext.cs
@@ -1,6 +1,6 @@
 ﻿using Explorer.Blog.Core.Domain;
 using Microsoft.EntityFrameworkCore;
-using BlogEntity = Explorer.Blog.Core.Domain.Blog;
+using BlogEntity = Explorer.Blog.Core.Domain.Blog; // "Blog is a namespace but is used as a type" -> fix for this, not really a good one but whatever for now..
 
 namespace Explorer.Blog.Infrastructure.Database;
 
