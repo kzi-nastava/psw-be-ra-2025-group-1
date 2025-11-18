5009b5fb2c130ab53252901e9b930206cc33f51c
Katarina Vujovic <v.katar551@gmail.com>
2025-11-18 20:36:30 +0100

    feat: Add ProfileControlle

---
+++ src/Explorer.API/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers
+{
+    [Authorize(Policy = "authorOrTouristPolicy")]
+    [Route ("api/users/profile")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+
+        public ProfileController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet("{id: long}")]
+        public ActionResult<PersonDto> Get(long id)
+        {
+            return Ok(_personService.Get(id));
+        }
+
+        [HttpPut("{id: long}")]
+        public ActionResult<PersonDto> UpdateProfile(long id, [FromBody] PersonDto personDto)
+        {
+            return Ok(_personService.Update(personDto));
+        }
+    }
+}
