84c331822339db596fdf0ed1877ebd36dfdc174e
Katarina Vujovic <v.katar551@gmail.com>
2025-11-18 20:48:33 +0100

    feat: Implement Person service and repository
    +Expand Authconfiguration policies with authorOrTouristPolicy

--- src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Repositories/PersonDbRepository.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Repositories/PersonDbRepository.cs
@@ -1,4 +1,5 @@
-﻿using Explorer.Stakeholders.Core.Domain;
+﻿using Explorer.BuildingBlocks.Core.Exceptions;
+using Explorer.Stakeholders.Core.Domain;
 using Explorer.Stakeholders.Core.Domain.RepositoryInterfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,6 +27,27 @@ public class PersonDbRepository : IPersonRepository
     {
         _dbSet.Add(entity);
         DbContext.SaveChanges();
+        return entity;
+    }
+
+    public Person Update(Person entity)
+    {
+        try
+        {
+            DbContext.Update(entity);
+            DbContext.SaveChanges();
+        }
+        catch (DbUpdateException e)
+        {
+            throw new NotFoundException(e.Message);
+        }
+        return entity;
+    }
+
+    public Person Get(long id)
+    {
+        var entity = _dbSet.Find(id);
+        if (entity == null) throw new NotFoundException("Not found: " + id);
         return entity;
     }
 }
