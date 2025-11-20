2183ea82bc6802e47180823d04014a03f64ee435
GameSpender <misa.herceg@gmail.com>
2025-11-20 07:23:47 +0100

    implement TourPreference entity

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/TourPreference.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class TourPreference
+    {
+        public int TouristId { get; init; }
+        public User? User { get; init; }
+        public double Difficulty { get; init; }
+        public int WalkRating { get; init; }
+        public int BicycleRating { get; init; }
+        public int CarRating { get; init; }
+        public int BoatRating { get; init; }
+
+        public List<string>? PreferedTags;
+    }
+}
