b632ed895e63505888e69c971fbb631523939361
Katarina Vujovic <v.katar551@gmail.com>
2025-11-30 19:54:31 +0100

    feat: Add Keypoint, KeypointDto and DbSet

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Keypoint.cs
@@ -0,0 +1,34 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class Keypoint : Entity
+    {
+        public string Title { get; init; }
+        public string? Description { get; init; }
+        public string? ImageUrl { get; init; }
+        public string? Secret { get; init; }
+
+        public Keypoint(string title, string description = null, string imageUrl = null, string secret = null)
+        {
+            Title = title;
+            Description = description;
+            ImageUrl = imageUrl;
+            Secret = secret;
+
+            Validate();
+        }
+
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Title)) throw new ArgumentException("Invalid title");
+        }
+    }
+}
--- src/Modules/Tours/Explorer.Tours.Infrastructure/Database/ToursContext.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/Database/ToursContext.cs
@@ -10,6 +10,7 @@ public class ToursContext : DbContext
     public DbSet<Tour> Tour { get; set; }
     public DbSet<Facility> Facility { get; set; }
     public DbSet<MeetUp> MeetUp { get; set; }
+    public DbSet<Keypoint> Keypoints { get; set; }
 
     public DbSet<PersonEquipment> PersonEquipment { get; set; } //dodala sam
 
