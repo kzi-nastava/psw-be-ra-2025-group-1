63de5401a23d858a63b2a0c141d37333162cf442
Tamas Papp <pap.ra4.2022@uns.ac.rs>
2025-12-01 16:58:05 +0100

    feat: Created TransportTime model and dto (Maybe TourTime in the future)

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TransportTimeDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public enum TransportTypeDto
+    {
+        Foot,
+        Bike,
+        Car,
+    }
+
+    public class TransportTimeDto
+    {
+        public long Id { get; set; }
+        public TransportTypeDto Type { get; set; }
+        public int Duration { get; set; }
+        public long TourId { get; set; }
+    }
+}
