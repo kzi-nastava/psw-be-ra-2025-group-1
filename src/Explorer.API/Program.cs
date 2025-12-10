75b6d2788ae12d76ac2515acacd8b989db766741
Emi7i <emilijaopsenica@gmail.com>
2025-12-10 12:47:56 +0100

    fix: Make program use the correct database
    The main DB is now created correctly
    The tests now do not add the data to main db, but instead use the test DB

--- src/Explorer.API/Program.cs
+++ src/Explorer.API/Program.cs
@@ -15,13 +15,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Set test database environment variables in development - COMMENTED OUT FOR MAIN DB ACCESS
 if (builder.Environment.IsDevelopment())
- {
-    Environment.SetEnvironmentVariable("DATABASE_SCHEMA", "explorer-v1-test");
-    Environment.SetEnvironmentVariable("DATABASE_HOST", "localhost");
-    Environment.SetEnvironmentVariable("DATABASE_PORT", "5432");
-    Environment.SetEnvironmentVariable("DATABASE_USERNAME", "postgres");
-    Environment.SetEnvironmentVariable("DATABASE_PASSWORD", "root");
- }
+{
+    // Only set test database if we're actually running tests
+    var isRunningTests = Environment.GetEnvironmentVariable("RUNNING_TESTS") == "true";
+
+    if (isRunningTests)
+    {
+        Environment.SetEnvironmentVariable("DATABASE_SCHEMA", "explorer-v1-test");
+        Environment.SetEnvironmentVariable("DATABASE_HOST", "localhost");
+        Environment.SetEnvironmentVariable("DATABASE_PORT", "5432");
+        Environment.SetEnvironmentVariable("DATABASE_USERNAME", "postgres");
+        Environment.SetEnvironmentVariable("DATABASE_PASSWORD", "root");
+    }
+}
 
 builder.Services.AddControllers();
 builder.Services.ConfigureSwagger(builder.Configuration);
