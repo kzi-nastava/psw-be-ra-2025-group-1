d338b3e7bb6064c25fcb95bca62a5f99a60c5c70
Katarina Vujovic <v.katar551@gmail.com>
2025-12-10 01:46:05 +0100

    feat: Fix keypoint tests

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/g-tour-execution.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/g-tour-execution.sql
@@ -2,6 +2,6 @@
 -- IMPORTANT: This runs AFTER e-tour.sql
 
 -- Update tour with ID -1 to be Published
-UPDATE tours."Tour" 
-SET "Status" = 1, "Price" = 100.0
-WHERE "Id" = -1;
+--UPDATE tours."Tour" 
+--SET "Status" = 1, "Price" = 100.0
+--WHERE "Id" = -1;
