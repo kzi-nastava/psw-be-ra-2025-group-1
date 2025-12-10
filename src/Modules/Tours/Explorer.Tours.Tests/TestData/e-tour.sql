b01b46dc73dda8a490dc9474718caa0604d6aba5
Katarina Vujovic <v.katar551@gmail.com>
2025-12-10 01:07:00 +0100

    fix: SQL tour script error

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/e-tour.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/e-tour.sql
@@ -1,9 +1,86 @@
 ﻿INSERT INTO tours."Tour"(
-	"Id", "CreatorId", "Title", "Description", "Difficulty", "Tags", "Status", "Price")
-	VALUES (-15, -1, 'Tour de Vojvodina', 'Best places in vojvodina', 1, ARRAY['Vojvodina','Serbia','Fun'], 0, 0.0);
+    "Id",
+    "CreatorId",
+    "Title",
+    "Description",
+    "Difficulty",
+    "Tags",
+    "Status",
+    "Price",
+    "CreatedAt",
+    "UpdatedAt",
+    "PublishedAt",
+    "ArchivedAt"
+)
+VALUES (
+    -15,
+    -1,
+    'Tour de Vojvodina',
+    'Best places in vojvodina',
+    1,
+    ARRAY['Vojvodina','Serbia','Fun'],
+    0,
+    0.0,
+    '2024-01-10T10:00:00Z',
+    '2024-01-10T10:00:00Z',
+    '2024-01-10T10:00:00Z',
+    '2024-01-10T10:00:00Z'
+);
+
 INSERT INTO tours."Tour"(
-	"Id", "CreatorId", "Title", "Description", "Difficulty", "Tags", "Status", "Price")
-	VALUES (-19, -2, 'Tour de Hungary', 'Best places in Hungary', 1, ARRAY['Vojvodina','Serbia','Fun'], 0, 1.0);
+    "Id",
+    "CreatorId",
+    "Title",
+    "Description",
+    "Difficulty",
+    "Tags",
+    "Status",
+    "Price",
+    "CreatedAt",
+    "UpdatedAt",
+    "PublishedAt",
+    "ArchivedAt"
+)
+VALUES (
+    -19,
+    -1,
+    'Tour de Hungary',
+    'Best places in Hungary',
+    1,
+    ARRAY['Vojvodina','Serbia','Fun'],
+    0,
+    1.0,
+    '2024-02-05T09:30:00Z',
+    '2024-02-05T09:30:00Z',
+    '2024-01-10T10:00:00Z',
+    '2024-01-10T10:00:00Z'
+);
+
 INSERT INTO tours."Tour"(
-	"Id", "CreatorId", "Title", "Description", "Difficulty", "Tags", "Status", "Price")
-	VALUES (-29, -3, 'Tour de Banat', 'Places...', 1, ARRAY['Vojvodina','Serbia','Fun'], 0, 0.0);
+    "Id",
+    "CreatorId",
+    "Title",
+    "Description",
+    "Difficulty",
+    "Tags",
+    "Status",
+    "Price",
+    "CreatedAt",
+    "UpdatedAt",
+    "PublishedAt",
+    "ArchivedAt"
+)
+VALUES (
+    -29,
+    -1,
+    'Tour de Banat',
+    'Places...',
+    1,
+    ARRAY['Vojvodina','Serbia','Fun'],
+    0,
+    0.0,
+    '2024-03-01T14:15:00Z',
+    '2024-03-01T14:15:00Z',
+    '2024-01-10T10:00:00Z',
+    '2024-01-10T10:00:00Z'
+);
