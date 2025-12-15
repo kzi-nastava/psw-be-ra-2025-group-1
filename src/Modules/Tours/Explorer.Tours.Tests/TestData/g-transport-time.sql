982f7a8f1642cd10cde2b748dd8e2bdfc8a79910
Tamas Papp <pap.ra4.2022@uns.ac.rs>
2025-12-15 17:00:18 +0100

    fix: changed tourid in seed script

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/g-transport-time.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/g-transport-time.sql
@@ -1,9 +1,9 @@
 INSERT INTO tours."TransportTime"(
 	"Id", "Type", "Duration", "TourId")
-	VALUES (-1, 2, 15, -15);
+	VALUES (-1, 2, 15, 1);
 INSERT INTO tours."TransportTime"(
 	"Id", "Type", "Duration", "TourId")
-	VALUES (-2, 1, 30, -15);
+	VALUES (-2, 1, 30, 1);
 INSERT INTO tours."TransportTime"(
 	"Id", "Type", "Duration", "TourId")
-	VALUES (-3, 0, 60, -19);
+	VALUES (-3, 0, 60, 2);
