ff0232200826440b3001362b2a5d6f06800f9940
GameSpender <misa.herceg@gmail.com>
2025-12-16 22:09:55 +0100

    add author-accessible api for equipment fetching

---
+++ src/Explorer.API/Controllers/Author/EquipmentController.cs
@@ -0,0 +1,27 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Explorer.API.Controllers.Author
+{
+    [Authorize(Policy = "authorPolicy")]
+    [Route("api/author/equipment")]
+    [ApiController]
+    public class EquipmentController : ControllerBase
+    {
+        private readonly IEquipmentService _equipmentService;
+
+        public EquipmentController(IEquipmentService equipmentService)
+        {
+            _equipmentService = equipmentService;
+        }
+        [HttpGet]
+        public ActionResult<PagedResult<EquipmentDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            return Ok(_equipmentService.GetPaged(page, pageSize));
+        }
+    }
+}
