1f64c707954284374a58ec6d15cc664e198e08b3
Katarina Vujovic <v.katar551@gmail.com>
2025-12-17 17:11:25 +0100

    fix: Test database

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/a-tour.sql
+++
@@ -1,9 +0,0 @@
-﻿INSERT INTO tours."Tour"(
-	"Id", "CreatorId", "Title", "Description", "Difficulty", "Tags", "Status", "Price")
-	VALUES (-15, -1, 'Tour de Vojvodina', 'Best places in vojvodina', 1, '{"Vojvodina", "Serbia", "Fun"}', 'Draft', 0);
-INSERT INTO tours."Tour"(
-	"Id", "CreatorId", "Title", "Description", "Difficulty", "Tags", "Status", "Price")
-	VALUES (-19, -2, 'Tour de Hungary', 'Best places in Hungary', 1, '{"Europe", "Hungary", "Fun"}', 'Draft', 1);
-INSERT INTO tours."Tour"(
-	"Id", "CreatorId", "Title", "Description", "Difficulty", "Tags", "Status", "Price")
-	VALUES (-29, -3, 'Tour de Banat', 'Places...', 1, '{"Vojvodina", "Serbia", "Not fun"}', 'Draft', 0);
