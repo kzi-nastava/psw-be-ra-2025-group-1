36bec82ed933d2f35450c3d886bdfe891e3534a6
Igor Glisovic <igorognjen6@gmail.com>
2026-01-13 18:50:43 +0100

    fix/syntax error

--- src/Explorer.Architecture.Tests/ModulesTests.cs
+++ src/Explorer.Architecture.Tests/ModulesTests.cs
@@ -97,7 +97,7 @@ public class ModulesTests : BaseArchitecturalTests
         {
             "Tours"
         },
-        new object[] { "Payments" }
+        new object[] { "Payments" },
         new object[]
         {
             "Encounters"
