5461ec3ee11b41c0b5f29e7a1d96742f03b816f2
Kristina Petrovic <petrovickristina718@gmail.com>
2026-01-26 15:41:32 +0100

    feat: Blog Collaborators

---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/BlogCollaborator.cs
@@ -0,0 +1,21 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using Explorer.Stakeholders.Core.Domain;
+
+namespace Explorer.Blog.Core.Domain
+{
+    public class BlogCollaborator : Entity
+    {
+        public long BlogId { get; private set; }
+        public long UserId { get; private set; }
+
+        public User? User { get; private set; }
+
+        protected BlogCollaborator() { }
+
+        public BlogCollaborator(long blogId, long userId)
+        {
+            BlogId = blogId;
+            UserId = userId;
+        }
+    }
+}
