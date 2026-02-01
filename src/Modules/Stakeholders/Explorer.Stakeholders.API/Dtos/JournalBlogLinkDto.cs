b681a70a60c72e39be312a87f298242b617b2365
Kristina Petrovic <petrovickristina718@gmail.com>
2026-02-01 11:08:48 +0100

    fix: fixovanje testova

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/JournalBlogLinkDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Explorer.Stakeholders.API.Dtos;
+
+public class JournalCollaboratorInfoDto
+{
+    public long UserId { get; set; }
+    public string Username { get; set; } = "";
+}
+
+public class JournalBlogLinkDto
+{
+    public long JournalId { get; set; }
+    public long BlogId { get; set; }
+    public List<JournalCollaboratorInfoDto> Collaborators { get; set; } = new();
+}
